9b09c76b9f84c7a4ab24cbfa0a4416a272745dd4
wisner..marques <wisner.a.marques@gmail.com>
2025-02-26 14:37:34 -0300

    token de autenticação

--- app_backend_tarefas/settings.py
+++ app_backend_tarefas/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
     'tarefas',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,13 +108,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+DEFAULT_CHARSET = 'utf-8'
 
 
 # Static files (CSS, JavaScript, Images)
@@ -126,10 +131,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  # Exemplo: frontend Flutter web
+    "http://127.0.0.1:8080",  # Exemplo: frontend Flutter web
+    "http://localhost:3000",  # Exemplo: frontend React
+    "https://seu-frontend.com",  # Substitua pelo domínio do seu frontend
+]
+
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,19 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Tarefa
 from .serializers import TarefaSerializer
 
-# Create your views here.
-
 # View para listar todas as tarefas e criar uma nova tarefa
 class TarefaListCreateView(generics.ListCreateAPIView):
-    queryset = Tarefa.objects.all()
-    serializer_class = TarefaSerializer
-
-class TarefaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
 
 # View para recuperar, atualizar e deletar uma tarefa específica
-class TarefaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class TarefaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
